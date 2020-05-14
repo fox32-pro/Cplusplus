8a32516ec2d0968b99ada6681717d3b419eeb91c
Soumen Samanta <soumensamanta.official@gmail.com>
2020-05-14 12:52:08 +0530

    First Commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+	char x[10],y[10];
+	cout << "Username : ";
+	cin >> x;
+	cout << "Password : ";
+	cin >> y;
+	cout << "Your Username is : " << x << endl;
+	cout << "Your Password is : " << y << endl;
+	getch(); //this will pause the code
+	//this code is for testing perpose so don't warry
+	//auther Soumen Samanta [Microfarad]
+	//code id : 1254
+	//msk : 543
+}
